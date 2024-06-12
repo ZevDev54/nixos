a530d9c28ab6b0140c2e95109ac7b13fa3e72285
ZevDev54 <zevy.johnston@gmail.com>
2024-06-12 09:40:03 -0600

    Initialize repo

---
+++ hosts/default/main-user.nix
@@ -0,0 +1,33 @@
+{lib, config, pkgs, ...}:
+
+let 
+  cfg = config.main-user;
+in
+{
+	options.main-user = {
+	  enable = lib.mkEnableOption "enable user module";
+
+	  userName = lib.mkOption {
+	  	default = "mainuser";
+	  	description = ''
+	  	username
+	  	'';
+	  };
+	};
+
+	config = lib.mkIf cfg.enable {
+		users.users.${cfg.userName} = {
+			isNormalUser = true;
+ 			initialPassword = "12345";
+			description = "Zev";
+			extraGroups = [ "networkmanager" "wheel" ];
+			shell = pkgs.fish;
+
+			packages = with pkgs; [
+				vscode
+				godot_4
+				neofetch
+			];	
+		};	
+	};
+}
