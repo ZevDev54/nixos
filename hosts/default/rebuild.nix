4e473585cfb6a0da9a977c772cb0aa6c88dedba1
ZevDev54 <zevy.johnston@gmail.com>
2024-06-28 09:21:52 -0600

    improve rebuild script

--- hosts/default/rebuild.nix
+++ hosts/default/rebuild.nix
@@ -1,5 +1,27 @@
 {pkgs }:
 
 pkgs.writeShellScriptBin "rebuild" ''
-	sudo nixos-rebuild switch --flake ~/dotfiles/nixos/flake.nix#default
+	set -e
+
+	pushd ~/dotfiles/nixos/
+
+	if git diff --quiet '*.nix'; then
+		echo "No changes detected, exiting."
+		popd
+		exit 0
+  fi
+
+	#git diff -U0 '*.nix'
+	echo "NixOS rebuilding..."
+
+	sudo nixos-rebuild switch --flake ~/dotfiles/nixos/flake.nix#default &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
+
+	current = $(nixos-rebuild list-generations | grep current)
+
+	git commit -am "$current"
+
+	popd
+
+	notify-send -e "NixOS rebuilt OK!"
+	
 ''
