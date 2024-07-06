3d455ba177cfd7b1838d4615224c79fb2004d56c
zev <zevy.johnston@gmail.com>
2024-07-05 19:06:23 -0600

    add the zevpc-vm host directory

---
+++ hosts/zevpc-vm/main-user.nix
@@ -0,0 +1,36 @@
+{lib, config, pkgs, ...}:
+
+let 
+  cfg = config.main-user;
+in
+{
+	options.main-user = {
+	  enable = lib.mkEnableOption "enable user module";
+
+	  userName = lib.mkOption {
+	  	default = "mainuser";
+	  	description = ''
+	  	Zev
+	  	'';
+	  };
+	};
+
+	config = lib.mkIf cfg.enable {
+		users.users.${cfg.userName} = {
+			isNormalUser = true;
+ 			initialPassword = "12345";
+			description = "Zev";
+			extraGroups = [ "networkmanager" "wheel" ];
+			shell = pkgs.fish;
+
+			packages = with pkgs; [
+				vscode
+				godot_4
+				neofetch
+				wget
+				htop
+				
+			];	
+		};	
+	};
+}
